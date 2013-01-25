34ec96f2c5d7fc26976d035ed4f4c9a2ba586020
Jens Raaby (adp) <jens.raaby@....adpeople.com>
2013-01-25 16:20:44 +0100

    hello @paddie

--- Misc/software-update.sh
+++ Misc/software-update.sh
@@ -5,5 +5,6 @@
 #
 #  Created by Jens Raaby on 10/9/12.
 #
+# Do not use on MacBooks, just on locally connected Macs!
 
 defaults write /Library/Preferences/com.apple.SoftwareUpdate CatalogURL http://cph41mini.local:8088/index.sucatalog
